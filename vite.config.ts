48e6d3df5af90734123db69d3e378de37356f762
ravisai2k <ravisai2k@gmail.com>
2026-09-09 16:59:30 +0530

    deployment script issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: isGitHubPages ? '/buildinghealth/' : '/',
+  base: isGitHubPages ? '/Company-Management-App/' : '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
